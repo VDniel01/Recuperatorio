8e9d192abba746c64e946b5af095031b4dced66d
VDniel01 <89489261+VDniel01@users.noreply.github.com>
2024-07-14 23:44:27 -0300

    arreglos

--- Assets/Scripts/Controller_Player_Inverted.cs
+++ Assets/Scripts/Controller_Player_Inverted.cs
@@ -7,27 +7,72 @@ public class Controller_Player_Inverted : Controller_Player
     protected override void Start()
     {
         base.Start();
-        Physics.gravity = new Vector3(0, 30, 0); // Invertir la gravedad
+        rb.useGravity = false; // Desactivar la gravedad predeterminada
     }
 
     protected override void FixedUpdate()
     {
+        if (GameManager.actualPlayer == playerNumber)
+        {
+            Movement();
+            // Aplicamos una fuerza continua hacia arriba para simular gravedad invertida
+            rb.AddForce(new Vector3(0, 30f, 0));
+        }
         base.FixedUpdate();
     }
 
     protected override void Update()
     {
-        base.Update();
+        if (GameManager.actualPlayer == playerNumber)
+        {
+            Jump();
+            canMoveLeft = !SomethingLeft();
+            canMoveRight = !SomethingRight();
+            canJump = IsOnSomething();
+        }
+        else
+        {
+            if (onFloor)
+            {
+                rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+            }
+            else
+            {
+                if (IsOnSomething() && downHit.collider != null && downHit.collider.gameObject.CompareTag("Player"))
+                {
+                    rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+                }
+            }
+        }
+    }
+
+    public override bool IsOnSomething()
+    {
+        // Comprobamos si hay algo sobre el jugador en lugar de debajo
+        return Physics.BoxCast(transform.position, new Vector3(transform.localScale.x * 0.9f, transform.localScale.y / 3, transform.localScale.z * 0.9f), Vector3.up, out downHit, Quaternion.identity, downDistanceRay);
     }
 
     public override void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && canJump)
         {
-            if (canJump)
-            {
-                rb.AddForce(new Vector3(0, -jumpForce, 0), ForceMode.Impulse); // Invertir la dirección del salto
-            }
+            rb.AddForce(new Vector3(0, -jumpForce, 0), ForceMode.Impulse); // Aplicar fuerza hacia abajo
+        }
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.A) && canMoveLeft)
+        {
+            rb.velocity = new Vector3(1 * -speed, rb.velocity.y, 0);
+        }
+        else if (Input.GetKey(KeyCode.D) && canMoveRight)
+        {
+            rb.velocity = new Vector3(1 * speed, rb.velocity.y, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
         }
     }
 }
